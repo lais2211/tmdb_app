fcdfb40ffdb2771298ab597393bc1c5ad8dd9340
lais2211 <lais.sousa1608@gmail.com>
2024-06-27 21:46:34 -0300

    feat: creation of l10n

---
+++ lib/app/modules/tmdb_app/l10n/movie_l10n.dart
@@ -0,0 +1,31 @@
+class MovieL10n {
+  String get appTitle => 'TMDB';
+
+  String get buttonSearch => 'Pesquise aqui';
+
+  String get buttonTrending => 'Tendências';
+
+  String get buttonPopular => 'Populares';
+
+  String get nowPlayingMovies => 'Lançamentos';
+
+  String get upComingMovies => 'Próximos lançamentos';
+
+  String get topRatedMovies => 'Melhor avaliação';
+
+  String get avaliation => 'Avaliação';
+
+  String get noDate => 'No data available';
+
+  String get cast => 'Cast';
+
+  String get sucess => 'Success';
+
+  String get notFound => 'Not Found';
+
+  String get serverError => 'Server Error';
+
+  String get unknownError => 'Unknown Error';
+
+  String get error => 'Error';
+}
