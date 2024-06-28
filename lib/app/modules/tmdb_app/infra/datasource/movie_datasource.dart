8dbe803c923c4b71029fc7a378cff8be0d8bc40b
lais2211 <lais.sousa1608@gmail.com>
2024-06-27 21:41:02 -0300

    feat: creation of datasource

---
+++ lib/app/modules/tmdb_app/infra/datasource/movie_datasource.dart
@@ -0,0 +1,21 @@
+import 'package:tmdb_app/app/modules/tmdb_app/infra/models/cast_model.dart';
+import '../models/movie_model.dart';
+
+abstract class MovieDatasource {
+  Future<List<MovieModel>> getUpcomingMovies(
+      {required String language, required int page});
+
+  Future<List<MovieModel>> getTopRatedMovies(
+      {required String language, required int page});
+
+  Future<List<MovieModel>> getNowPlayingMovies(
+      {required String language, required int page});
+
+  Future<List<MovieModel>> getPopularMovies(
+      {required String language, required int page});
+
+  Future<List<MovieModel>> searchMovies(
+      {required String language, required String search, required int page});
+
+  Future<List<CastModel>> getCast({required int movieId});
+}
