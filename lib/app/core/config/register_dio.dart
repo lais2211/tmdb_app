21933f4103b30e179de5b5a9d5bd2f0aaf64103d
lais2211 <lais.sousa1608@gmail.com>
2024-06-29 05:14:26 -0300

    feat: interceptor for dio creation

--- lib/app/core/config/register_dio.dart
+++ lib/app/core/config/register_dio.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
 
+import 'authorization_interceptor.dart';
 import 'config_env.dart';
 
 Dio registerDio() {
   final dio = Dio();
+  dio.interceptors.add(AuthorizationInterceptor());
   dio.options.baseUrl = ConfigEnv.basePath;
   dio.options.queryParameters = {'api_key': ConfigEnv.apiKey, 'language': 'pt-BR'};
 
